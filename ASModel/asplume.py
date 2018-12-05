f29988409c208ce76f782401e7d250a2c8ab99e8
secretyv <yves.secretan@ete.inrs.ca>
2018-12-05 12:57:01 -0500

    Class d'échange d'information pour un tracé de particule

---
+++ ASModel/asplume.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#************************************************************************
+# --- Copyright (c) Yves Secretan 2018
+# ---
+# --- Licensed under the Apache License, Version 2.0 (the "License");
+# --- you may not use this file except in compliance with the License.
+# --- You may obtain a copy of the License at
+# ---
+# ---     http://www.apache.org/licenses/LICENSE-2.0
+# ---
+# --- Unless required by applicable law or agreed to in writing, software
+# --- distributed under the License is distributed on an "AS IS" BASIS,
+# --- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# --- See the License for the specific language governing permissions and
+# --- limitations under the License.
+#************************************************************************
+
+"""
+"""
+
+import logging
+
+LOGGER = logging.getLogger("INRS.ASModel.plume")
+
+class ASPlume(object):
+    """
+    Structure to hold all information pertaining to a
+    particle path (plume)
+    """
+    def __init__(self, dilution=-1.0, name='', poly=None, tide=(), t0=-1, tc=-1, isDirect=False, plume=None):
+        self.dilution       = dilution  # 
+        self.stationName    = name      # string
+        self.stationPolygon = poly      # sequence of (x, y) 
+        self.tide           = tide      # (tide duration [s], tide amplitude [m])
+        self.injectionTime  = t0        # datetime
+        self.contactTime    = tc        # datetime
+        self.isPlumeDirect  = isDirect  # Bool
+        self.plume          = plume
+
+    def __str__(self):
+        return 'Station: %s; tide: %s; t0: %s' % (self.stationName, self.tide, self.injectionTime)
+        
+    def __repr__(self):
+        s = []
+        s.append('%.2e' % self.dilution)
+        s.append('%s' % self.stationName)
+        s.append('%s' % (self.tide,))
+        s.append(':')
+        s.append('t_inj=%s' % self.injectionTime)
+        s.append('t_hit=%s' % self.contactTime)
+        s.append('direct=%s' % self.isPlumeDirect)
+        s.append('plume=%d' % len(self.plume))
+        return ' '.join(s)
