57149660e8079e3a6daafe52d0561260229ff388
secretyv <yves.secretan@ete.inrs.ca>
2018-12-14 10:16:28 -0500

    MAJ Cosmétique

--- ASModel/__about__.py
+++ ASModel/__about__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-__version__      = "18.12"
+__version__      = "18.12rc1"
 __name__         = "ASModel"
 __author__       = "Yves Secretan (previously INRS - Centre ETE)"
 __author_email__ = "Yves.Secretan@ete.inrs.ca"
-__copyright__    = "Copyright (c) 2016 INRS\nCopyright (c) 2018 Yves Secretan"
+__copyright__    = "Copyright (c) 2016 {}\nCopyright (c) 2018 {}".format('INRS', 'Yves Secretan')
 __license__      = "License :: Apache License, Version 2.0"
 __status__       = "Development Status :: 5 - Production/Stable"
