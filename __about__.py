7479b3359906e21fed386da816aeec04495e16da
secretyv <yves.secretan@ete.inrs.ca>
2018-12-14 11:26:05 -0500

    Cosmétique

--- __about__.py
+++ __about__.py
@@ -3,6 +3,6 @@
 __version__      = "2.0rc1"
 __author__       = "Yves Secretan (previously INRS - Centre ETE)"
 __author_email__ = "Yves.Secretan@ete.inrs.ca"
-__copyright__    = "Copyright (c) 2016, {}\nCopyright (c) 2018 {}".format('INRS', 'Yves Secretan')
+__copyright__    = "Copyright (c) 2016 {}\nCopyright (c) 2018 {}".format('INRS', 'Yves Secretan')
 __license__      = "License :: Apache License, Version 2.0"
 __status__       = "Development Status :: 5 - Production/Stable"
