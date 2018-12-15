a12b85e2d8eb3d5467a062ae0b49665f84038066
secretyv <yves.secretan@ete.inrs.ca>
2018-12-14 19:35:45 -0500

    Copie gdal_array

--- _pyinstaller-Asur.bat
+++ _pyinstaller-Asur.bat
@@ -1,4 +1,7 @@
 
 :: LD_LIBRARY_PATH=/opt/anaconda3/lib:$LD_LIBRARY_PATH
 pyinstaller --noconfirm ASur.spec
+
+:: Fix osgeo
 cp dist/ASur/osgeo._gdal.pyd dist/ASur/_gdal.pyd
+cp dist/ASur/_gdal_array.pyd dist/ASur/osgeo._gdal_array.pyd
