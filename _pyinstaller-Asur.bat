688ba79ce7052fe7821be1df12d0ee5058ef1829
secretyv <yves.secretan@ete.inrs.ca>
2018-12-15 13:28:57 -0500

    Obsolete

--- _pyinstaller-Asur.bat
+++
@@ -1,7 +0,0 @@
-
-:: LD_LIBRARY_PATH=/opt/anaconda3/lib:$LD_LIBRARY_PATH
-pyinstaller --noconfirm ASur.spec
-
-:: Fix osgeo
-cp dist/ASur/osgeo._gdal.pyd dist/ASur/_gdal.pyd
-cp dist/ASur/_gdal_array.pyd dist/ASur/osgeo._gdal_array.pyd
--- install-all.bat
+++
@@ -1,9 +0,0 @@
-﻿setlocal
-
-call setup_python27_32.bat
-call setup_vc_x86_for_python.bat
-
-:: ---  Pyinstaller 3.1 (3.2 est buggé)
-pyinstaller --version-file=Pa2Beau_version.txt PaBeau-all.spec
-
-:eoj
