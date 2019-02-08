8aab77b1f681ea5788e08cbc3a645713a47862cf
secretyv <yves.secretan@ete.inrs.ca>
2019-02-08 13:31:25 -0500

    Bump la version à 2.0p1

--- __about__.py
+++ __about__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-__version__      = "2.0"
+__version__      = "2.0p1"
 __author__       = "Yves Secretan (previously INRS - Centre ETE)"
 __author_email__ = "Yves.Secretan@ete.inrs.ca"
 __copyright__    = "Copyright (c) 2016 {}\nCopyright (c) 2018 {}".format('INRS', 'Yves Secretan')
